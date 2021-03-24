d6ed9af29e71ed2b808e5e269a5c4245b0b112b8
lucasgcb <lucascoliado@hotmail.com>
2021-03-24 20:18:02 -0300

    Add github API docstring

--- vough_backend/api/integrations/github.py
+++ vough_backend/api/integrations/github.py
@@ -1,8 +1,31 @@
+"""GitHub API
+
+This module contains a class that hits the public GitHub API to search for organizations
+using the provided given login name, and is also has a function to return the number of
+public members of that organization.
+
+It utilize the standard requests library.
+
+"""
 import os
 import requests
 
 
 class GithubApi:
+    """
+    API for returning pythonified GitHub organization data from the official GitHub API in json.
+
+    ...
+
+    Methods
+    -------
+    get_organization(request,  login: str)
+        Attempts to retrieve the GitHub organization data with the given
+        login name.
+    get_organization(request,  login: str)
+        Attempts to retrieve the number of GitHub organization members with the given
+        login name.
+    """
     API_URL = os.environ.get("GITHUB_API_URL", "https://api.github.com")
     GITHUB_TOKEN = os.environ.get("GITHUB_TOKEN")
 
@@ -11,13 +34,13 @@ class GithubApi:
 
         :login: login da organização no Github
         """
-        r = requests.get(self.API_URL + "/orgs/" + login)
-        return r
+        response = requests.get(self.API_URL + "/orgs/" + login)
+        return response
 
     def get_organization_public_members(self, login: str) -> int:
         """Retorna todos os membros públicos de uma organização
 
         :login: login da organização no Github
         """
-        r = requests.get(self.API_URL + "/orgs/" + login + "/members")
-        return len(r.json())
+        response = requests.get(self.API_URL + "/orgs/" + login + "/members")
+        return len(response.json())
