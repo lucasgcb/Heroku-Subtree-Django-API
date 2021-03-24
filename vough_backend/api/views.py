5cebcfa797eb15174c95260fed6fa27c1f72ea9c
lucasgcb <lucascoliado@hotmail.com>
2021-03-24 20:18:11 -0300

    Add org view docstring

--- vough_backend/api/views.py
+++ vough_backend/api/views.py
@@ -1,10 +1,14 @@
-from rest_framework import viewsets, status
+"""
+Views for the vough application.
+
+"""
+
+from rest_framework import viewsets
 from rest_framework.views import Response
 from django.http import Http404
 from api import models, serializers
 from api.integrations.github import GithubApi
 from api.models import Organization
-import json
 
 # TODOS:
 # 1 - Buscar organização pelo login através da API do Github
@@ -14,23 +18,34 @@ import json
 
 
 class OrganizationViewSet(viewsets.ModelViewSet):
+    """
+    Viewset for returning, scrapping and deleting API information of GitHub organizations.
 
-    queryset = models.Organization.objects.all().order_by('-score')
+    ...
+
+    Methods
+    -------
+    retrieve(request, login=None)
+        Attempts to retrieve, return and store GitHub organizations with the matching login.
+        Returns json on hit.
+        Throws 404 on miss.
+    """
+    # Ordenar por ordem decrescente (maior para menor)
+    queryset = models.Organization.objects.all().order_by('-score')  # pylint: disable=no-member
     serializer_class = serializers.OrganizationSerializer
     lookup_field = "login"
-
     def retrieve(self, request, login=None):
-        API = GithubApi()
-        r = API.get_organization(login)
-        if r.ok: 
-            json = r.json()
+        api = GithubApi()
+        response = api.get_organization(login)
+        if response.ok:
+            json = response.json()
             try:
-                name = json["name"]
-            except:
+                name = json["name"] # Resposta por vir sem name!
+                #Ex: /api/orgs/rebellionifsc
+            except KeyError:
                 name = json["login"]
-            
-            score = json["public_repos"] + API.get_organization_public_members(login)
-            b = Organization(login=json["login"], name=name, score=score)
-            b.save()
+            score = json["public_repos"] + api.get_organization_public_members(login)
+            organization_update = Organization(login=json["login"], name=name, score=score)
+            organization_update.save()
             return Response(json)
         raise Http404
